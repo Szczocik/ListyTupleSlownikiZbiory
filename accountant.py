f74f628a5de05bd5073bd8dfa45b09d3c3246b08
popam <popa.mariusz@gmail.com>
2021-08-25 16:55:50 +0200

    dodanie import sys

---
+++ accountant.py
@@ -0,0 +1,8 @@
+import sys
+print('python accountant.py')
+#komenda_wywolania = sys argv [1]
+lista_komend_dozwolonych = ('saldo', 'sprzedaz', 'zakup', 'konto', 'magazyn', 'przeglad')
+lista_komend_dozwolonych = ('python accountant.py saldo', 'sprzedaz', 'zakup', 'konto', 'magazyn', 'przeglad')
+
+#while lista_komend_dozwolonych:
+ #   if saldo

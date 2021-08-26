9f8fdd702940da95249ab3906e6c798b869f8f52
popam <popa.mariusz@gmail.com>
2021-08-26 12:50:53 +0200

    nowy zapis

--- accountant.py
+++ accountant.py
@@ -1,8 +1,10 @@
 import sys
-print('python accountant.py')
-#komenda_wywolania = sys argv [1]
-lista_komend_dozwolonych = ('saldo', 'sprzedaz', 'zakup', 'konto', 'magazyn', 'przeglad')
-lista_komend_dozwolonych = ('python accountant.py saldo', 'sprzedaz', 'zakup', 'konto', 'magazyn', 'przeglad')
+#lista_komend_dozwolonych = sys.argv[1]
+komenda =
+#lista_komend_dozwolonych = ('saldo', 'sprzedaz', 'zakup', 'konto', 'magazyn', 'przeglad')
+#lista_komend_dozwolonych = ('python accountant.py saldo', 'python accountant.py sprzedaz', 'python accountant.py zakup',\n
+#'python accountant.py konto', 'python accountant.py magazyn', 'python accountant.py przeglad')
+#magazyn = {}
 
-#while lista_komend_dozwolonych:
- #   if saldo
+while
+
--- main.py
+++ main.py
@@ -1,4 +1,24 @@
-name = input("What is your name: ")
-age = int(input("How old are you: "))
-year = str((2021 - age)+100)
-print(name + " will be 100 years old in the year " + year)
+import sys
+
+job_count = int(sys.argv[1])
+workload1 = 0
+workload2 = 0
+workload3 = 0
+for job in range(job_count):
+    print("Podaj liczbę godzin pracy dla zlecenia")
+    job_hours = int(input())
+    if job_hours <= 0:
+        print("Błąd: Nieprawidłowa liczba godzin!")
+        break
+    if workload3 < workload1 and workload3 < workload2:
+        workload3 += job_hours
+    elif workload2 < workload1:
+        workload2 += job_hours
+    else:
+        workload1 += job_hours
+print("Najbliży mechanik będzie wolny w ciągu {} dni".format(
+    int((min(workload1, workload2, workload3)+7)  / 8)
+))
+print("Wszyscy mechanicy będą wolni w ciągu {} dni".format(
+    int((max(workload1, workload2, workload3)+7)  / 8)
+))
